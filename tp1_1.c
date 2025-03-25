1ce20d29aaefcb874953805ae46aef671020c82f
JuanPabloGonzalez16 <juanpablofacultad16@hotmail.com>
2025-03-25 16:45:39 -0230

    Commit codigo

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,13 @@
 
 int main(){
     printf("Hola mundo");
+    int a = 8;
+    int *p;
+    p = &a;
+    printf("\nContenido del puntero : %d", *p);
+    printf("\nValor al que apunta el puntero: %d",p);
+    printf("\nDireccion de memoria de la varibale a: %d",&a);
+    printf("\nDireccion de memoria del puntero: %d",&p);
+    printf("\nTamaño de la variable a: %d", sizeof(a));
     return 0;
 }
